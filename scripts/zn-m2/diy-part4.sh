f8b01792cd5c3c0c63d70e6bcd54636bde3e15a4
xdm2211 <127907289+xdm2211@users.noreply.github.com>
2025-12-10 17:26:50 +0800

    Create diy-part4.sh
    IP 10.0.0.1

---
+++ scripts/zn-m2/diy-part4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part4.sh
+# Description: OpenWrt DIY script part 3 (After Install feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+#修改版本信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
+# 替换golang版本为1.25
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+# ttyd免登陆
+sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config

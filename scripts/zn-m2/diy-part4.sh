5cf6fd06ab61614371a253d9b8cec282325b72d8
xdm2211 <127907289+xdm2211@users.noreply.github.com>
2025-12-10 18:31:29 +0800

    修改默认IP为10.0.0.1

--- scripts/zn-m2/diy-part4.sh
+++ scripts/zn-m2/diy-part4.sh
@@ -7,11 +7,11 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part4.sh
-# Description: OpenWrt DIY script part 3 (After Install feeds)
+# Description: OpenWrt DIY script part 4 (After Install feeds)
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
